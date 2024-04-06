05ba490caeabbc1fd38b1c79998912c203a9042e
Gabriel Magioli <gabrielmagioli1@gmail.com>
2024-04-05 22:59:58 -0300

    Feat: implemented the Unity test for Cards

--- src/entities/cards.test.ts
+++ src/entities/cards.test.ts
@@ -1,19 +1,26 @@
 
-
-// id: props.id,
-// card_number: props.card_number,
-// security_number: props.security_number,
-// owner: props.owner,
-// password: props.password,
-// generatedAt: props.generatedAt,
-// expiresAt: props.expiresAt//define a date of 2 years validation 
-
+import { expect, test } from "vitest";
 import { Cards } from "./cards";
+import { User } from "./user";
 
 test("should create a new card", () => {
+
+    const user: User = new User({
+        id: 1,
+        email: "johndoe@email.com",
+        name: "John Doe",
+        password: "123456",
+      });
+
     const card: Cards = new Cards({
         id: 1,
         card_number: "12321431",
         security_number: "123",
+        owner: user,
+        password: "121212",
+        generatedAt: new Date(),
+        expiresAt: new Date()
     });
+
+    expect(card).instanceOf(Cards);
 })
