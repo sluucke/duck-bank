f2d89a2292f5c3674157f0399fc4de26c30229d4
David William <sluuckejoohn@gmail.com>
2024-04-13 22:06:15 -0300

    feat(encrypt-provider): add unit test and fix crypto provider

--- src/providers/encrypt/crypto/Crypto.ts
+++ src/providers/encrypt/crypto/Crypto.ts
@@ -4,21 +4,19 @@ import { EncryptProvider } from "../EncryptProvider";
 export class CryptoProvider implements EncryptProvider {
   async hash(text: string): Promise<string> {
     const salt = crypto.randomBytes(16).toString("hex");
-    const hash = Buffer.from(
-      crypto.hkdfSync("sha512", text, salt, "info", 64)
-    ).toString("hex");
+    const hash = crypto
+      .pbkdf2Sync(text, salt, 1000, 64, "sha512")
+      .toString("hex");
 
-    return hash;
+    return `${salt}:${hash}`;
   }
 
   async verify(text: string, hashText: string): Promise<boolean> {
-    const originalHash = Buffer.from(hashText, "hex");
+    const [salt, hash] = hashText.split(":");
+    const hashVerify = crypto
+      .pbkdf2Sync(text, salt, 1000, 64, "sha512")
+      .toString("hex");
 
-    const salt = originalHash.slice(0, 16).toString("hex");
-    const hash = Buffer.from(
-      crypto.hkdfSync("sha512", text, salt, "info", 64)
-    ).toString("hex");
-
-    return hash === hashText;
+    return hash === hashVerify;
   }
 }
---
+++ src/providers/encrypt/crypto/crypto.test.ts
@@ -0,0 +1,18 @@
+import { describe, expect, it } from "vitest";
+import { CryptoProvider } from "./Crypto";
+
+const cryptoProvider = new CryptoProvider();
+describe("Crypto Encrypter", () => {
+  it("should encrypt a value", async () => {
+    const hashedValue = cryptoProvider.hash("any_value");
+
+    expect(hashedValue).not.toBe("any_value");
+  });
+
+  it("should compare values", async () => {
+    const hashedValue = await cryptoProvider.hash("any_value");
+    const isValid = await cryptoProvider.verify("any_value", hashedValue);
+
+    expect(isValid).toBe(true);
+  });
+});
